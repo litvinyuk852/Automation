df8275b26f24984ff96971e6a05bb27523819f83
litvinyuk852 <anastasiyalitvinyuk@gmail.com>
2018-10-29 21:44:25 +0300

    final

--- TaskThreeCalculator/src/Application.java
+++ TaskThreeCalculator/src/Application.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 public class Application {
-    int num1;
-    int num2;
-    int result;
-
+    float num1;
+    float num2;
+    float result;
+    char operator;
     public static void main(String[] args) {
         Application calculator=new Application();
-        //try {
             Scanner in = new Scanner(System.in);
-            System.out.println("Input numbers");
+            System.out.println("Input number, operator, number.");
             while (in.hasNext()){
                 String s = in.next();
                 if("quit".equals(s)){
@@ -16,61 +15,48 @@ public class Application {
                 }
                 calculator.num1 = calculator.getNum1(s);
                 calculator.num2 = calculator.getNum2(s);
-                char op = calculator.getOperator(s);
-                switch (op){
+                calculator.operator= calculator.getOperator(s);
+                switch (calculator.operator){
                     case '+':
                         calculator.calcAddition();
+                        calculator.printResult();
                         break;
                     case '-':
                         calculator.calcSubtraction();
+                        calculator.printResult();
+                        break;
+                    case '*':
+                        calculator.calcMultiplication();
+                        calculator.printResult();
+                        break;
+                    case '/':
+                        calculator.calcDivision();
+                        calculator.printResult();
                         break;
                     default:
-                        System.out.println("Operator is incorrect");
+                        System.out.println("Operator is absent or incorrect.");
                         break;
                 }
-                System.out.println(calculator.result);
-                /*calculator.num1 = in.nextInt();
-                System.out.println("Input operator.");
-                calculator.operator = in.next();
-                if(calculator.operator.equals("+") || calculator.operator.equals("-")) {
-                    System.out.println("Input second number.");
-                    calculator.num2 = in.nextInt();
-                    if (calculator.operator.equals("+")) {
-                        calculator.calcAddition();
-                    }
-                    System.out.println(calculator.result);
-                    if (calculator.operator.equals("-")) {
-                        calculator.calcSubtraction();
-                    }
-                    System.out.println(calculator.result);
-                }else{
-                    System.out.println("Неверный оператор");
-                }*/
             }
-        //}catch (Exception e){
-            //System.out.println("Only numbers should be input");
-        //}
     }
-
-
-    private int getNum1(String example){
+    private float getNum1(String example){
         try {
             String result = "";
             for (char c : example.toCharArray()) {
-                if (c == '+' || c == '-') {
+                if (c == '+' || c == '-' || c == '*' || c == '/') {
                     break;
                 }
                 result = result + c;
             }
-            int i = Integer.parseInt(result);
+            float i = Float.parseFloat(result);
             return i;
         }catch (Exception e){
-            System.out.println("Only numbers should be input");
+            System.out.println("First number is absent or not digital.");
         }
         return 0;
     }
 
-    private int getNum2(String example){
+    private float getNum2(String example){
         try {
             boolean wasOperator = false;
             String result = "";
@@ -78,35 +64,42 @@ public class Application {
                 if(wasOperator){
                     result = result + c;
                 }
-                if (c == '+' || c == '-') {
+                if (c == '+' || c == '-' || c == '*' || c == '/') {
                     wasOperator = true;
                 }
-
             }
-            int i = Integer.parseInt(result);
+            float i = Float.parseFloat(result);
             return i;
         }catch (Exception e){
-            System.out.println("Only numbers should be input");
+            System.out.println("Second number is absent or not digital.");
         }
         return 0;
     }
-
     private char getOperator(String example){
         char result = 0;
         for (char c : example.toCharArray()) {
-            if (c == '+' || c == '-') {
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
                 result = c;
             }
         }
         return result;
     }
-
-
     private void calcAddition() {
         result = num1 + num2;
     }
-
-    void calcSubtraction() {
+    private void calcSubtraction() {
         result = num1 - num2;
+    }
+    private void calcMultiplication() {
+        result = num1 * num2;
+    }
+    private void calcDivision() {
+        if (num2==0) {
+            System.out.print("Division by 0 can not be fulfilled.");
+        }
+        result=num1/num2;
+    }
+    private void printResult() {
+        System.out.println(result);
     }
 }
